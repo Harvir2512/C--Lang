7776386e8f5a5a4f7b73a61468181caf1b98c8f2
Harvir2512 <harveervarma10@gmail.com>
2026-02-09 01:29:34 -0800

    Add files via upload

---
+++ prime_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("enter number :");
+	scanf("%d",&n);
+	for(i=2; i<=n; i++)
+	{
+		if(n%i==0)
+		{
+			break;
+		}
+	}
+	if(i==n)
+	{
+		printf("prime number");
+	}
+	else
+	{
+		printf("not prime number");
+	}
+}
